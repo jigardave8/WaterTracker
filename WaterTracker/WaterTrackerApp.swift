ef9c2eda9fdd3764f24f8883541988d73efaa60d
BitDegree <jdavenz8@gmail.com>
2025-07-08 17:23:04 +0530

    Initial Commit

---
+++ WaterTracker/WaterTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTrackerApp.swift
+//  WaterTracker
+//
+//  Created by BitDegree on 08/07/25.
+//
+
+import SwiftUI
+
+@main
+struct WaterTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
